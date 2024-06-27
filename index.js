920346c9e2bb47d6adaac730f585adffc08c9b72
annewaithaka <annewaithaka68@gmail.com>
2024-06-27 22:25:08 +0300

    complete strings lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+// const welcomeMessage = 'Welcome to Flatbook, ';
+// const welcomeMessage = currentUser;
+// const welcomeMessage = 'Welcome to Flatbook, '+ currentUser;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// console.log (excitedWelcomeMessage);
+// const shortGreeting = 'Welcome, ';
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
